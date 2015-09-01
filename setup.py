49b7a52965228fcfed3053d14653ea3a33834c05
Dmitry Orlov <me@mosquito.su>
2015-09-01 13:35:10 +0300

    [fix] bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, print_function
 from setuptools import setup, find_packages
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Dmitry Orlov <me@mosquito.su>'
 
 
